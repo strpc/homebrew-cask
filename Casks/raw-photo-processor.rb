992e96ddf54b419d98c387cb01fbd2672d3ce848
strpc <56894629+strpc@users.noreply.github.com>
2019-11-06 00:30:31 +0300

    Update raw-photo-processor.rb

--- Casks/raw-photo-processor.rb
+++ Casks/raw-photo-processor.rb
@@ -1,6 +1,6 @@
 cask 'raw-photo-processor' do
-  version '1876Beta'
-  sha256 '458d9912db08ba1d874bed49242a51e2d01f32220ecc07cc7449fd5352e8bf3d'
+  version '1877Beta'
+  sha256 'fc0ba1d5305fb04370be71130c3821cfccae9c8966f0abd8feae812c21cec597'
 
   url "https://www.raw-photo-processor.com/RPP/RPP64_#{version}.zip"
   appcast 'https://www.raw-photo-processor.com/rpp_updates_64.xml'
